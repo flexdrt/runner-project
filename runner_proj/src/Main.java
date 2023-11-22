b710eeefed628844933340a0d5dcee435bd678d0
FlexDirty <vincent.lakhmeche@ensea.fr>
2023-11-22 10:11:18 +0100

    hello world fonctionnel

--- runner_proj/src/Main.java
+++ runner_proj/src/Main.java
@@ -1,17 +1,23 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.layout.Pane;
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("Hello world");
+        Group root = new Group();
+        Pane pane = new Pane(root);
+        Scene theScene = new Scene(pane, 600, 400, true);
+        primaryStage.setScene(theScene);
+        primaryStage.show();
+    }
+
+
     public static void main(String[] args) {
-        // Press Alt+Entrée with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        launch(args);
     }
 }
+
---
+++ runner_proj/unofficial_readme.txt
@@ -0,0 +1,1 @@
+ceci est le répertoire créé par intellij lorsque j'ai créée le projet et il contient tous les fichiers de code de mon projet
